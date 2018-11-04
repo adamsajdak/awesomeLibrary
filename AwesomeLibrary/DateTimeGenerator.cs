55d6a6ea9d8ee1a7ddb043cace1529fcaa6b2dbf
Adam Sajdak <adam.sajdak@onehydra.com>
2018-11-04 14:56:21 +0100

    Add for loop

--- AwesomeLibrary/DateTimeGenerator.cs
+++ AwesomeLibrary/DateTimeGenerator.cs
@@ -6,6 +6,13 @@ namespace AwesomeLibrary
     {
         public string Generate(bool isLongDate)
         {
+            var n = 9999999999;
+            for (var i = 0; i < n; i++)
+            {
+                var x = 1;
+
+                var j = x + i;
+            }
             if(isLongDate)
             {
                 return DateTime.Now.ToLongDateString();
