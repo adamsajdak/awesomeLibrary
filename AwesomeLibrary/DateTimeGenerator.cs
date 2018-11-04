6079cb3c52fd283b41adcd22edc6438a282ecbc9
Adam Sajdak <adam.sajdak@onehydra.com>
2018-11-04 12:25:28 +0100

    Add initial project

---
+++ AwesomeLibrary/DateTimeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AwesomeLibrary
+{
+    public class DateTimeGenerator
+    {
+        public string Generate(bool isLongDate)
+        {
+            if(isLongDate)
+            {
+                return DateTime.Now.ToLongDateString();
+            }
+            return DateTime.Now.ToShortDateString();
+        }
+    }
+}
